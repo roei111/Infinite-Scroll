731efc89078895065a5659278e13c49598fc5fc4
RoeiY <roeiyaacobi@gmail.com>
2022-01-19 14:01:27 +0200

    19.1.22

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import ScrollTop from "./ScrollTop";
 import SearchForm from "./SearchForm";
 import PhotoList from "./PhotosList";
@@ -6,15 +6,9 @@ import NavBar from "./NavBar";
 
 //Enable smooth scroll for ios devices
 import smoothscroll from "smoothscroll-polyfill";
+import FetchImages from "./FetchImages";
 smoothscroll.polyfill();
 
-//Unsplash limits their free api to 50 requests per hour, so I opened 12 'projects' to bypass the limit, and this random helps to switch between the keys
-const randomNum = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-//Access key and urls to Unsplash API
-const access = `REACT_APP_ACCESS_KEY${randomNum}`;
-const clientID = `?client_id=${process.env[access]}`;
-const mainUrl = `https://api.unsplash.com/photos/`;
-const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -28,138 +22,34 @@ const App = () => {
     message: "",
     isPrevPhotos: false,
   });
-  const [addNewPhotos, setAddNewPhotos] = useState(false)
-
-  //When some filter variable changes, the function runs with 'resetData' variable sets to true.
-  const fetchImages = async (resetData = false) => {
-    setLoading(true);
-    //Set the url params
-    let url;
-    const urlPage = `&page=${page}`;
-    const urlQuery = `&query=${query}`;
-    const urlSortBy = `&order_by=${sortBy}`;
-    const urlOrientation =
-      orientation !== "any" ? `&orientation=${orientation}` : "";
-    const urlColor = color !== "any" ? `&color=${color.toLowerCase()}` : "";
-    //Switch between search for any photo to search with specific filters and query
-    url = query
-      ? `${searchUrl}${clientID}${urlPage}${urlQuery}${urlSortBy}${urlOrientation}${urlColor}`
-      : `${mainUrl}${clientID}${urlPage}`;
-    try {
-      const response = await fetch(url);
-      //In this api status code 403 means you've reached the maximum requests per hour so when this happends, I refresh the page to get new access key with 50 more requests
-      if (response.status === 403) {
-        window.location.reload();
-      }
-      const data = await response.json();
-      console.log("data: ",data)
-      //Checks if there is no results at all, or no more results, and sets the message
-      if (data.results && data.results.length === 0) {
-        if (data.total === 0) {
-          if (resetData) {
-            setNoResultsMessage({
-              message: `Sorry, we could'nt find anything for "${query}" with your filters`,
-              isPrevPhotos: false,
-            });
-          } else {
-            setNoResultsMessage({
-              message: `Sorry, we could'nt find anything for "${query}"`,
-              isPrevPhotos: false,
-            });
-          }
-        } else
-          setNoResultsMessage({
-            message: "Sorry, there is no more results :(",
-            isPrevPhotos: true,
-          });
-      } else {
-        //Set the new photos according to the parameters
-        setPhotos((prevValue) => {
-          if (query && page === 1) {
-            return data.results;
-          } else if (!query && resetData) {
-            return data;
-          } else if (query && resetData) {
-            return data.results;
-          } else if (query) {
-            return [...prevValue, ...data.results];
-          }
-          return [...prevValue, ...data];
-        });
-        console.log("in the else resetinig the message");
-        setNoResultsMessage({ message: "", isPrevPhotos: false });
-      }
-      setLoading(false);
-      setAddNewPhotos(false);
-    } catch (error) {
-      setLoading(false);
-      setAddNewPhotos(false);
-      setNoResultsMessage({
-        message:
-          "There is a limit of 50 searches per hour, please try again later.",
-        isPrevPhotos: false,
-      });
-    }
-  };
-  //
-  useEffect(() => {
-    fetchImages();
-    // eslint-disable-next-line
-  }, [page]);
-
-  useEffect(() => {
-    if(!addNewPhotos) return;
-    if(loading) return;
-    console.log("changing the page");
-    setPage((prevValue) => {
-      if (prevValue === 0) {
-        return prevValue + 2;
-      }
-      return prevValue + 1;
-    });
-    // eslint-disable-next-line
-  }, [addNewPhotos]);
-
-  //when some filter variable changes, the fetchImages function runs with 'resetData' variable sets to true.
-  useEffect(() => {
-    fetchImages(true);
-    // eslint-disable-next-line
-  }, [sortBy, orientation, color]);
-
-  useEffect(() => {
-    //event listener that checks if the user scrolled to down to the end of the page
-    console.log(noResultsMessage.message);
-    if (noResultsMessage.message !== "") {
-      console.log("returning");
-      return;
-    }
-    if (loading) {
-      console.log("loading... return");
-      return;
-    }
-    const event = window.addEventListener("scroll", () => {
-      if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
-        setAddNewPhotos(true);
-        
-        
-      }
-    });
-    return () => window.removeEventListener("scroll", event);
-    // eslint-disable-next-line
-  }, [noResultsMessage]);
+  const [addNewPhotos, setAddNewPhotos] = useState(false);
 
   const handleSubmit = (e) => {
     //reset the photos data and run fetchImages function
     e.preventDefault();
     setPhotos([]);
     setPage(1);
-    fetchImages();
   };
 
   return (
     <>
       <NavBar />
       <main style={{ margin: "0.5rem" }}>
+        <FetchImages
+          loading={loading}
+          setLoading={setLoading}
+          page={page}
+          setPage={setPage}
+          query={query}
+          sortBy={sortBy}
+          orientation={orientation}
+          color={color}
+          noResultsMessage={noResultsMessage}
+          setNoResultsMessage={setNoResultsMessage}
+          setPhotos={setPhotos}
+          addNewPhotos={addNewPhotos}
+          setAddNewPhotos={setAddNewPhotos}
+        />
         <SearchForm
           handleSubmit={handleSubmit}
           query={query}
